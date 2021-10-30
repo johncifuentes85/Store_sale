99eede22d2b21d6dc8edfbf9a3fc0bbc7bc4a009
johncifuentes85 <john.alexcz@gmail.com>
2021-10-29 19:07:25 -0500

    se termina el crud

--- app/src/main/java/com/example/store_sale/AddProductActivity.java
+++ app/src/main/java/com/example/store_sale/AddProductActivity.java
@@ -4,6 +4,7 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -12,13 +13,27 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.store_sale.databinding.ActivityAddProductBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AddProductActivity extends AppCompatActivity {
+
+    Button btnImage, btnAddProduct;
+    EditText etName,etDescription, etStock, etPrice, etCategory;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     private ActivityAddProductBinding addProductBinding;
     private FirebaseStorage storage;
@@ -35,6 +50,14 @@ public class AddProductActivity extends AppCompatActivity {
         setContentView(v);
         storage = FirebaseStorage.getInstance();
         storageReference = storage.getReference();
+
+        btnAddProduct = findViewById(R.id.btnAddProduct);
+        btnImage = findViewById(R.id.btnImage);
+        etName = findViewById(R.id.etProductName);
+        etDescription = findViewById(R.id.etProductDescription);
+        etStock = findViewById(R.id.etProducStock);
+        etPrice = findViewById(R.id.etProductPrice);
+        etCategory = findViewById(R.id.etProducCategory);
     }
 
     public void selectImageFromGallery(View view){
@@ -73,5 +96,47 @@ public class AddProductActivity extends AppCompatActivity {
         Intent intent = new Intent(this,InicioActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    public void addProduct(View view){
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Creando producto");
+        progressDialog.show();
+
+        Map<String, Object> userData = new HashMap<>();
+
+        String name = etName.getText().toString();
+        String description = etDescription.getText().toString();
+        int stock = Integer.parseInt(etStock.getText().toString());
+        double price = Double.parseDouble(etPrice.getText().toString());
+        String category = etCategory.getText().toString();
+
+        userData.put("name",name);
+        userData.put("description",description);
+        userData.put("stock",stock);
+        userData.put("price",price); // se envia la informacion
+        userData.put("category",category);
+
+        db.collection("products")
+                .add(userData)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        //Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                        Toast.makeText(getApplicationContext(), "Producto Creado", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //Log.w(TAG, "Error adding document", e);
+                        Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        Intent intent = new Intent(getApplicationContext(),InicioActivity.class);
+        startActivity(intent);
+        finish();
+
     }
 }
--- app/src/main/java/com/example/store_sale/EditProductActivity.java
+++ app/src/main/java/com/example/store_sale/EditProductActivity.java
@@ -1,14 +1,21 @@
 package com.example.store_sale;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.store_sale.Entities.Product;
 import com.example.store_sale.databinding.ActivityEditProductBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class EditProductActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -23,15 +30,44 @@ public class EditProductActivity extends AppCompatActivity implements View.OnCli
         editProductBinding = ActivityEditProductBinding.inflate(getLayoutInflater());
         View view = editProductBinding.getRoot();
         setContentView(view);
+        editProductBinding.btnUpdate.setOnClickListener(this);
         Intent intent = getIntent();
         product = (Product) intent.getSerializableExtra("product");
         db = FirebaseFirestore.getInstance();
+        editProductBinding.etName.setText(product.getName());
+        editProductBinding.etDescription.setText(product.getDescription());
+        editProductBinding.etStock.setText(String.valueOf(product.getStock()));
+        editProductBinding.etPrice.setText(String.valueOf(product.getPrice()));
+        editProductBinding.etCategory.setText(product.getCategory());
     }
 
     @Override
     public void onClick(View v) {
+        Map<String, Object> dataProduct = new HashMap<>();
+        dataProduct.put("name", editProductBinding.etName.getText().toString());
+        dataProduct.put("description", editProductBinding.etDescription.getText().toString());
+        dataProduct.put("stock", Integer.parseInt(editProductBinding.etStock.getText().toString()));
+        dataProduct.put("price", Double.parseDouble(editProductBinding.etPrice.getText().toString()));
+        dataProduct.put("category", editProductBinding.etCategory.getText().toString());
         if(v.getId() == editProductBinding.btnUpdate.getId()){
-
+            db.collection("products")
+                    .document(product.getId())
+                        .update(dataProduct)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(getApplicationContext(), "Product update", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(),ListProductActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(), "Error updating", Toast.LENGTH_SHORT).show();
+                        }
+                    });
         }
     }
 
